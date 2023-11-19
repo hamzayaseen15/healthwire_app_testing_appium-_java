8ae49399239e8cec57b920c3c25d501b372985ee
hamzayaseen15 <hamzayaseen993@gmail.com>
2023-11-20 01:11:55 +0500

    fixes

--- src/test/java/stepdefinitions/pharmacy_feature.java
+++ src/test/java/stepdefinitions/pharmacy_feature.java
@@ -8,12 +8,19 @@ import io.cucumber.java.en.And;
 import org.example.DriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
+import java.util.List;
+import java.util.Random;
 
 
 public class pharmacy_feature {
     AppiumDriver driver = DriverManager.getDriver();
+    int timeoutInSeconds = 30;
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeoutInSeconds));
+
+
 
     @Given("I click on order medicine button")
     public void clickOrderMedicineButton() {
@@ -25,53 +32,51 @@ public class pharmacy_feature {
 
     @Then("I add the address on google map")
     public void addAddressOnGoogleMap() {
-        // Implementation of the step...
+        WebElement google_map = driver.findElement(By.id("com.healthwire.healthwire:id/btnSave"));
+        google_map.click();
     }
 
     @When("I select the categories")
     public void selectCategories() {
-        // Implementation of the step...
+        WebElement el2 = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("com.healthwire.healthwire:id/rvHealthProductsByCategories")));
+        List<WebElement> top_med_ctg = el2.findElements(By.id("com.healthwire.healthwire:id/mainLayout"));
+        int random_top_med_ctg = new Random().nextInt(top_med_ctg.size());
+        WebElement select_category = top_med_ctg.get(random_top_med_ctg);
+        select_category.click();
     }
 
     @And("I click on search bar")
     public void clickOnSearchBar() {
-        // Implementation of the step...
+        WebElement search_medicine = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("com.healthwire.healthwire:id/tvSearchBar")));
+        search_medicine.click();
     }
 
     @Then("I Select two medicines from the search")
     public void selectMedicinesFromSearch() {
-        // Implementation of the step...
     }
 
     @Then("I Navigate back and click on view cart")
     public void navigateBackAndViewCart() {
-        // Implementation of the step...
     }
 
     @Then("I verify the calculation on cart page")
     public void verifyCalculationOnCartPage() {
-        // Implementation of the step...
     }
 
     @Then("I click on proceed to checkout button")
     public void clickOnProceedToCheckout() {
-        // Implementation of the step...
     }
 
     @Then("I select the address and scroll to the medicine information section")
     public void selectAddressAndScroll() {
-        // Implementation of the step...
     }
 
     @Then("I verify the medicine calculation")
     public void verifyMedicineCalculation() {
-        // Implementation of the step...
     }
 
     @Then("I click on order now button to place order")
     public void clickOrderNowButton() {
-        // Implementation of the step...
     }
-
 
 }
